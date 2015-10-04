054220cbb5878098dbd3399b7e2f71c32c01d4ce
Decade1 <leonardvonkn@gmail.com>
2015-10-04 23:25:52 +0200

    Final

--- Password checker/Password checker/Password_checker.py
+++ Password checker/Password checker/Password_checker.py
@@ -1,9 +1,8 @@
 pw = raw_input("Pleas type in the password to check: ")
 
-if pw.count(" ") >= 3:
-    print "Here I kneel before you, oh mighty Passlord! \nYou have a strong password. "
-elif len(pw) <= 4 or pw.islower() or pw.isupper() or pw.isdigit():
+if len(pw) <= 4 or pw.islower() or pw.isupper() or pw.isdigit():
     print "Waaaaaaaaaaaaaay too weak (and probably short). \nTry adding both capitalized and lower case letters."
+elif pw.count(" ") >= 3:
+    print "Here I kneel before you, oh mighty Passlord! \nYou have a strong password. "
 else:
     print "You're getting there! \nYour password is of medium strength."
-
